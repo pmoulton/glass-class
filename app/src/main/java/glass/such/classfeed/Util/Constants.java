f335f859c3843bcc6ed424873aa1d3de9cc3063b
Vincente <vciancio@socaldevs.com>
2014-06-21 00:47:49 -0700

    fix debug problem

--- app/src/main/java/glass/such/classfeed/Util/Constants.java
+++ app/src/main/java/glass/such/classfeed/Util/Constants.java
@@ -4,7 +4,7 @@ package glass.such.classfeed.Util;
  * Created by vincente on 6/20/14.
  */
 public class Constants {
-    class Debug{
+    public class Debug{
         public static final boolean ENABLED = true;
         public static final String WSURI    = "ws://192.168.43.129:8080/";
     }
