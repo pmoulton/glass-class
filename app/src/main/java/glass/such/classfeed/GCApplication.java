14d6aec9e772abc09fd82308dbf1d63488c06348
sciencepro <dcarr@scienceprousa.com>
2014-06-20 23:20:21 -0700

    stuff

--- app/src/main/java/glass/such/classfeed/GCApplication.java
+++ app/src/main/java/glass/such/classfeed/GCApplication.java
@@ -2,12 +2,21 @@ package glass.such.classfeed;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.radiusnetworks.proximity.ibeacon.startup.BootstrapNotifier;
+import com.radiusnetworks.proximity.ibeacon.startup.RegionBootstrap;
+import com.radiusnetworks.ibeacon.Region;
 
 /**
  * Created by vincente on 6/20/14.
  */
-public class GCApplication extends Application {
+public class GCApplication extends Application implements BootstrapNotifier {
 
+    private static final String TAG = "GCApplication";
+    private static final String beaconUUID = "B0702880-A295-A8AB-F734-031A98A512DE";
+    private RegionBootstrap regionBootstrap;
     private static Context mContext = null;
     private static AutoBahnConnection webSocketConnection = null;
 
@@ -15,6 +24,8 @@ public class GCApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+        Region region = new Region("com.radiusnetworks.androidproximityreference.backgroundRegion",beaconUUID, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
 
     }
 
@@ -22,6 +33,23 @@ public class GCApplication extends Application {
         if(webSocketConnection== null)
             webSocketConnection = new AutoBahnConnection();
         return webSocketConnection;
+    }
+
+    @Override
+    public void didEnterRegion(Region arg0) {
+        Log.d(TAG, "did enter region.");
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
     }
 
     public static Context getContext(){
