7f48ab6a0b41cba196cb55ba7ec22a8d85009409
sciencepro <dcarr@scienceprousa.com>
2014-06-20 23:09:47 -0700

    add radius

--- app/src/main/java/glass/such/classfeed/MainActivity.java
+++ app/src/main/java/glass/such/classfeed/MainActivity.java
@@ -1,17 +1,23 @@
 package glass.such.classfeed;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import glass.such.classfeed.R;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
     }
 
 
